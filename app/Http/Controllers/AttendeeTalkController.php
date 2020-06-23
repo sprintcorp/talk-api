089492acea4ee930e97dc5f21cee058dad5b1709
sprintcorp <sprintcorp7@gmail.com>
2020-06-23 13:40:37 -0700

    Project setup:Laravel installation,Model,controller and database migration creation

---
+++ app/Http/Controllers/AttendeeTalkController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\AttendeeTalk;
+use Illuminate\Http\Request;
+
+class AttendeeTalkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\AttendeeTalk  $attendeeTalk
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AttendeeTalk $attendeeTalk)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\AttendeeTalk  $attendeeTalk
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AttendeeTalk $attendeeTalk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\AttendeeTalk  $attendeeTalk
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AttendeeTalk $attendeeTalk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\AttendeeTalk  $attendeeTalk
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AttendeeTalk $attendeeTalk)
+    {
+        //
+    }
+}
